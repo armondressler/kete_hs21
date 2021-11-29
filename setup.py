8d0132fb85060105218aaf4fcca42c7d6112d97d
armondressler <armon.dressler@gmail.com>
2021-11-29 21:09:46 +0100

    basic django project

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='kete_hs21',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='Apache 2.0',
+    author='41796',
+    author_email='armon.dressler@gmail.com',
+    description='key technologies school project'
+)
