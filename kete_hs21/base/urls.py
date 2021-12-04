aeeff344b2fb8811d2f3600ef0e2d4623cbfcf3f
armondressler <armon.dressler@gmail.com>
2021-12-04 03:29:20 +0100

    working on basic structure

--- kete_hs21/base/urls.py
+++ kete_hs21/base/urls.py
@@ -1,30 +1,13 @@
-"""kete_hs21 URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.urls import path
-
 from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 
-from django.contrib.auth.views import LoginView,LogoutView
 urlpatterns = [
-    #path('dashboard/', views.dashboardView, name="dashboard"),
+    path('dashboard', views.dashboard, name="dashboard"),
     path('login', LoginView.as_view(), name="login_url"),
     #path('register/', views.registerView, name="register_url"),
     path('logout', LogoutView.as_view(next_page='dashboard'), name="logout"),
-    path('home', views.home, name="home"),
     path('welcome', views.welcome, name="home"),
+    path('about', views.about, name="about"),
     path('', views.root, name="roots")
 ]
--- kete_hs21/base/views.py
+++ kete_hs21/base/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect
 
-def home(request):
-    return render(request, "base/base.html")
-
 def welcome(request):
     return render(request, "welcome/welcome.html")
 
+def dashboard(request):
+    return render(request, "dashboard/dashboard.html")
+
 def root(request):
-    return redirect(welcome)
+    return redirect(dashboard)
+
+def about(request):
+    return redirect("https://github.com/armondressler/kete_hs21/blob/main/README.md")
