2f3316f07d87578896bc20365528d495ce4eedf2
armondressler <armon.dressler@gmail.com>
2021-12-12 23:52:58 +0100

    working on lessons

--- kete_hs21/lesson/urls.py
+++ kete_hs21/lesson/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.lessons, name="lessons"),
     path("create", views.create, name="lessons_create"),
+    path("details", views.details, name="lessons_details"),
     path("<int:lesson_id>/delete", views.delete, name="lessons_delete")
 ]
